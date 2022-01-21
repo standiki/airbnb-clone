2905c798ee5a00d9bba71af0a054e7f172773cfe
stanflows <atakistanley@gmail.com>
2022-01-21 23:12:53 +0100

    Image alt attribute fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Hero() {
     return (
         <section className="hero">
-            <img src={process.env.PUBLIC_URL + "assets/photo-grid.png"} className="hero--photo"></img>
+            <img src={process.env.PUBLIC_URL + "assets/photo-grid.png"} alt="" className="hero--photo"></img>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
